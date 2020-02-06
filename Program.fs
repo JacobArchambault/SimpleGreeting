085f772e938d258ea0afd84605bd6cd2e920b142
Jacob Archambault <jacob.archambault@waystar.com>
2020-02-06 08:37:37 -0500

    Add project files.

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿let getGreeting name = 
+    sprintf "Hello %s, isn't F# great?" name
+
+[<EntryPoint>]
+let main _ =
+    let names = ["Don"; "Julia"; "Xi"]
+
+    names
+    |> List.map getGreeting
+    |> List.iter (fun greeting -> printfn "%s" greeting)
+
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
